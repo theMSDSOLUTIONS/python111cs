fb3d3bfeeec031f3f257d14f872595f2beb82662
a4h4ak <4hm4dak2006@gmail.com>
2025-12-04 22:26:45 +0300

    main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 #HELLO DR'TEMRAZ WELCOME TO OUR CODE OF A CENIMA TICKETS WINDOW PROGRAM
-
-
+nigga=input("How many seats does your family want mate?:")
+for i in range (nigga):
+    seat=input("pick the seat number",i+1)
